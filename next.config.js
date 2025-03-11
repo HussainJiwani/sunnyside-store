b7634b70472c523ccdfe21ff5fb2943b2588e400
hussain.jiwani@cimpress.com <hussain.jiwani@cimpress.com>
2025-03-11 15:07:23 +0530

    Fixed admin subdomain routing

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
+  async redirects() {
     return [
       {
         source: "/",
         destination: "/admin/dashboard",
         has: [{ type: "host", value: "admin.sunnysidecountrystore.com" }],
+        permanent: true,
       },
     ];
   },
 };
 
 module.exports = nextConfig;
+
